80930a1fbf7aedd19388e59bf7f54c40dfa65ab7
Pavan Edirisingha <96134390+PavanEdirisingha2000@users.noreply.github.com>
2023-09-13 23:54:45 +0530

    GPA Calculater
    GPA Calculator App in Flutter
    
    This Flutter code represents a GPA (Grade Point Average) calculator application. The app allows users to input their grades and credit values for multiple subjects, dynamically add or remove subjects, and calculate their GPA based on the provided data.
    
     Key Features
    
    1. Subject Representation**: The `Subject` class is defined to represent each subject, with properties for `grade` (defaulted to 'A' or 4.0) and `credit` (defaulted to 3.0).
    
    2. Dynamic Subject Management**: Users can dynamically add or remove subjects. By default, it starts with one subject and supports up to 100 subjects.
    
    3. Grade Selection**: For each subject, users can select their grade from a dropdown list that includes grades A, A-, B+, B, B-, C+, and C.
    
    4. Credit Input**: Users can input credit values for each subject using text fields. The default credit value is 3.0.
    
    5. GPA Calculation**: The app calculates the GPA based on the provided grades and credits. It ensures proper handling of optional values and calculates the weighted average.
    
    6. User Interface**: The user interface includes a clean design with a scrollable list of subjects, each with a grade dropdown, a credit input field, and buttons to add or remove subjects. There's also an option to calculate the GPA, which displays the result in an alert dialog.
    
    Code Structure
    
    - The app is structured as a Flutter stateful widget, `GPACalculatorPage`, which manages the app's state.
    
    - It uses a `List` of `Subject` objects to store subject data and a `List` of `TextEditingController` instances to manage the credit input fields.
    
    - Proper input validation and disposal of controllers are implemented to prevent memory leaks.
    
    How to Run
    
    To run the app, execute the `main()` function in the code. The app will appear with the title "GPA Calculator," and users can interact with it to calculate their GPA.
    
    This code is a versatile foundation for building a GPA calculator app in Flutter and can be further enhanced with additional features or UI improvements as needed.

---
+++ gpa_calculater/lib/gpaCalculaterPage.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+// Custom class to represent a subject
+class Subject {
+  double? grade;
+  double? credit;
+
+  Subject({this.grade = 4.0, this.credit = 3.0}); // Set default grade to 'A' (4.0) and credit to 3.0
+}
+
+class GPACalculatorPage extends StatefulWidget {
+  const GPACalculatorPage({super.key});
+
+  @override
+  _GPACalculatorPageState createState() => _GPACalculatorPageState();
+}
+
+class _GPACalculatorPageState extends State<GPACalculatorPage> {
+  int currentSubject = 1; // Set the default subject value to 1
+  List<Subject> subjects = List.generate(100, (_) => Subject()); // Default to 'A' and 3.0 credits for 100 subjects
+
+  // Create a list of TextEditingController instances to control the credit text fields
+  List<TextEditingController> creditControllers = List.generate(100, (_) => TextEditingController(text: '3.0'));
+
+  double calculateGPA() {
+    double totalGradePoints = 0;
+    double totalCredits = 0;
+
+    // Loop through the subjects
+    for (int i = 0; i < currentSubject; i++) {
+      totalGradePoints += (subjects[i].grade ?? 0) * (subjects[i].credit ?? 0);
+      totalCredits += subjects[i].credit ?? 0;
+    }
+
+    return totalCredits == 0 ? 0 : totalGradePoints / totalCredits;// Return the GPA
+  }
+
+  // Function to add a new subject
+  void addSubject() {
+    if (currentSubject < 100) {
+      setState(() {
+        currentSubject++;
+      });
+    }
+  }
+
+  // Function to remove a subject
+  void removeSubject(int index) {
+    if (currentSubject > 1) {
+      setState(() {
+        currentSubject--;
+        subjects.removeAt(index);
+        creditControllers.removeAt(index);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the TextEditingController instances to avoid memory leaks
+    for (final controller in creditControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GPA Calculator'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Enter Grades and Credits in Subjects',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              for (int i = 0; i < currentSubject; i++)// Loop through the subjects
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: DropdownButton<double?>(
+                        value: subjects[i].grade,
+                        onChanged: (value) {
+                          setState(() {
+                            subjects[i].grade = value;// Set the grade value
+                          });
+                        },
+                        items: const [
+                          DropdownMenuItem(value: 4.0, child: Text('A')),
+                          DropdownMenuItem(value: 3.7, child: Text('A-')),
+                          DropdownMenuItem(value: 3.3, child: Text('B+')),
+                          DropdownMenuItem(value: 3.0, child: Text('B')),
+                          DropdownMenuItem(value: 2.7, child: Text('B-')),
+                          DropdownMenuItem(value: 2.3, child: Text('C+')),
+                          DropdownMenuItem(value: 2.0, child: Text('C')),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      flex: 2,
+                      child: TextField(
+                        controller: creditControllers[i], // Use the respective controller
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(labelText: 'Credits'),// Credits label
+                        onChanged: (value) {
+                          setState(() {
+                            subjects[i].credit = double.tryParse(value) ?? 0;// Parse the credit value
+                          });
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: () {
+                        removeSubject(i);
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        addSubject();
+                      },
+                    ),
+                  ],
+                ),
+              ElevatedButton(
+                onPressed: () {
+                  double gpa = calculateGPA();// Calculate GPA
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('GPA Calculation'),// GPA Calculation title
+                      content: Text('Your GPA is: ${gpa.toStringAsFixed(2)}'),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: const Text('Calculate GPA'),// Calculate GPA button
+              ),
+              const SizedBox(height: 10),
+              Text('Current Subject: $currentSubject'), // Display current subject counter
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(home: GPACalculatorPage()));
+}
